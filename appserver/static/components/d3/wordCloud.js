3e5e469a75ee61f6836cfe6bf93bb4d97079f89e
sawyerzhu <sawyerzhu@126.com>
2014-09-10 14:47:04 +0800

    commit timp

--- appserver/static/components/d3/wordCloud.js
+++ appserver/static/components/d3/wordCloud.js
@@ -68,25 +68,56 @@ define(function(require, exports, module) {
               .on("end", draw)
               .start();
 
-            function draw(words) {
-                me.$el.html("");
+            function draw(data, bounds) {
+              var w = 450,
+                  h = 450;
+              var scale = bounds ? Math.min(
+                  w / Math.abs(bounds[1].x - w / 2),
+                  w / Math.abs(bounds[0].x - w / 2),
+                  h / Math.abs(bounds[1].y - h / 2),
+                  h / Math.abs(bounds[0].y - h / 2)) / 2 : 1;
+              words = data;
 
-                d3.select(me.el).append("svg")
-                    .attr("width", 450)
-                    .attr("height", 450)
-                  .append("g")
-                    .attr("transform", "translate(225,225)")
-                  .selectAll("text")
-                    .data(words)
-                  .enter().append("text")
-                    .style("font-size", function(d) { return (d.size * 2) + "px"; })
-                    .style("font-family", "Impact")
-                    .style("fill", function(d, i) { return fill(i); })
-                    .attr("text-anchor", "middle")
-                    .attr("transform", function(d) {
-                      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-                    })
-                    .text(function(d) { return d.text; });
+              var vis = d3.select(me.el).append("svg")
+                  .attr("width", 450)
+                  .attr("height", 450)
+                .append("g")
+                  .attr("transform", "translate(225,225)");
+
+              var text = vis.selectAll("text")
+                  .data(words, function(d) { return d.text.toLowerCase(); });
+              text.transition()
+                  .duration(1000)
+                  .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; })
+                  .style("font-size", function(d) { return d.size + "px"; });
+              text.enter().append("text")
+                  .attr("text-anchor", "middle")
+                  .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; })
+                  .style("font-size", function(d) { return d.size + "px"; })
+                  .on("click", function(d) {
+                    load(d.text);
+                  })
+                  .style("opacity", 1e-6)
+                .transition()
+                  .duration(1000)
+                  .style("opacity", 1);
+              text.style("font-family", function(d) { return d.font; })
+                  .style("fill", function(d) { return fill(d.text.toLowerCase()); })
+                  .text(function(d) { return d.text; });
+              var exitGroup = background.append("g")
+                  .attr("transform", vis.attr("transform"));
+              var exitGroupNode = exitGroup.node();
+              text.exit().each(function() {
+                exitGroupNode.appendChild(this);
+              });
+              exitGroup.transition()
+                  .duration(1000)
+                  .style("opacity", 1e-6)
+                  .remove();
+              vis.transition()
+                  .delay(1000)
+                  .duration(750)
+                  .attr("transform", "translate(" + [w >> 1, h >> 1] + ")scale(" + scale + ")");
             }
         }
     });
