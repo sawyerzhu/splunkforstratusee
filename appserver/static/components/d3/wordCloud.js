3c6d675279343944135335b0659646010f3bf202
sawyerzhu <sawyerzhu@126.com>
2014-09-15 16:27:31 +0800

    commit

--- appserver/static/components/d3/wordCloud.js
+++ appserver/static/components/d3/wordCloud.js
@@ -65,33 +65,53 @@ define(function(require, exports, module) {
 
             function draw(words, bounds) {
                 var scale = bounds ? Math.min(
-                    w / Math.abs(bounds[1].x - w / 2),
-                    w / Math.abs(bounds[0].x - w / 2),
-                    h / Math.abs(bounds[1].y - h / 2),
-                    h / Math.abs(bounds[0].y - h / 2)) / 2 : 1;
+                        w / Math.abs(bounds[1].x - w / 2),
+                        w / Math.abs(bounds[0].x - w / 2),
+                        h / Math.abs(bounds[1].y - h / 2),
+                        h / Math.abs(bounds[0].y - h / 2)) / 2 : 1;
 
-                var vis = d3.select(me.el).append("svg")
-                  .attr("width", w)
-                  .attr("height", h)
-                .append("g")
-                  .attr("transform", "translate(" + Math.round(w / 2) + "," + Math.round(h / 2) + ")");
+                    var svg = d3.select(me.el).append("svg")
+                        .attr("width", w)
+                        .attr("height", h);
 
-                vis.selectAll("text")
-                  .data(words)
-                .enter().append("text")
-                  .style("font-size", function(d) { return d.size * 2 + "px"; })
-                  .style("font-family", "Impact")
-                  .style("fill", function(d, i) { return fill(i); })
-                  .attr("text-anchor", "middle")
-                  .attr("transform", function(d) {
-                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-                  })
-                  .text(function(d) { return d.text; });
-            
-                vis.transition()
-                  .delay(1000)
-                  .duration(750)
-                  .attr("transform", "translate(" + [w >> 1, h >> 1] + ")scale(" + scale + ")");
+                    var background = svg.append("g");
+                    var vis = svg.append("g")
+                        .attr("transform", "translate(" + [w >> 1, h >> 1] + ")");
+
+                    var text = vis.selectAll("text")
+                        .data(words, function(d) { return d.text.toLowerCase(); });
+                    text.transition()
+                        .duration(1000)
+                        .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; })
+                        .style("font-size", function(d) { return d.size + "px"; });
+                    text.enter().append("text")
+                        .attr("text-anchor", "middle")
+                        .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; })
+                        .style("font-size", function(d) { return d.size + "px"; })
+                        .on("click", function(d) {
+                          load(d.text);
+                        })
+                        .style("opacity", 1e-6)
+                      .transition()
+                        .duration(1000)
+                        .style("opacity", 1);
+                    text.style("font-family", function(d) { return d.font; })
+                        .style("fill", function(d) { return fill(d.text.toLowerCase()); })
+                        .text(function(d) { return d.text; });
+                    var exitGroup = background.append("g")
+                        .attr("transform", vis.attr("transform"));
+                    var exitGroupNode = exitGroup.node();
+                    text.exit().each(function() {
+                      exitGroupNode.appendChild(this);
+                    });
+                    exitGroup.transition()
+                        .duration(1000)
+                        .style("opacity", 1e-6)
+                        .remove();
+                    vis.transition()
+                        .delay(1000)
+                        .duration(750)
+                        .attr("transform", "translate(" + [w >> 1, h >> 1] + ")scale(" + scale + ")");
 
             }
         }
