121b2bd7c0786f1118d759f4f88c2f55ef2ed6b4
sawyerzhu <sawyerzhu@126.com>
2014-09-17 17:00:23 +0800

    commit tmp

--- appserver/static/auto_discover.js
+++ appserver/static/auto_discover.js
@@ -9,7 +9,7 @@ require(['splunkjs/mvc/simplexml/ready!'], function(){
     // set init config parameter
     ////////////////////////////
     var SplunkDModel = require('models/SplunkDBase');
-    var sharedModls = require('splunkjs/mvc/sharedmodels');
+    var sharedModels = require('splunkjs/mvc/sharedmodels');
 
     var CustomConfModel = SplunkDModel.extend({
         urlRoot: 'configs/conf-myconf'
